e47f45567b4f584586fca1ccb1c82a6b7f9b64d5
ulfschneider <mail@ulf.codes>
2022-05-16 13:22:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const spawn = require('cross-spawn');
+const commitCache = new Map();
+
+//get the latest commit date for the given file path
+//if the file is not committed yet, return null
+export function commitDate(filePath) {
+
+    let commitDate = commitCache.get(filePath);
+    if (commitDate) {
+        return commitDate;
+    } else {
+        commitDate = null;
+    }
+    try {
+        commitDate = new Date(parseInt(spawn.sync(
+            'git',
+            ['log', '-1', '--format=%at', path.basename(filePath)],
+            { cwd: path.dirname(filePath) }
+        ).stdout.toString('utf-8')) * 1000);
+    } catch (e) { 
+        console.log(e);
+    }
+    if (commitDate) {
+        commitCache.set(filePath, commitDate);
+    }
+    return commitDate;
+}
+
+export function clearCache() {
+    commitCache.clear();
+}
