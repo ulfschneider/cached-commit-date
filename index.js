73a43982287111ec7fd480bf739413f46a2bb8b5
ulfschneider <mail@ulf.codes>
2022-05-16 13:29:34 +0200

    Proper export of functions

--- index.js
+++ index.js
@@ -4,29 +4,32 @@ const commitCache = new Map();
 
 //get the latest commit date for the given file path
 //if the file is not committed yet, return null
-export function commitDate(filePath) {
 
-    let commitDate = commitCache.get(filePath);
-    if (commitDate) {
+module.exports = {
+    commitDate: function (filePath) {
+
+        let commitDate = commitCache.get(filePath);
+        if (commitDate) {
+            return commitDate;
+        } else {
+            commitDate = null;
+        }
+        try {
+            commitDate = new Date(parseInt(spawn.sync(
+                'git',
+                ['log', '-1', '--format=%at', path.basename(filePath)],
+                { cwd: path.dirname(filePath) }
+            ).stdout.toString('utf-8')) * 1000);
+        } catch (e) {
+            console.log(e);
+        }
+        if (commitDate) {
+            commitCache.set(filePath, commitDate);
+        }
         return commitDate;
-    } else {
-        commitDate = null;
-    }
-    try {
-        commitDate = new Date(parseInt(spawn.sync(
-            'git',
-            ['log', '-1', '--format=%at', path.basename(filePath)],
-            { cwd: path.dirname(filePath) }
-        ).stdout.toString('utf-8')) * 1000);
-    } catch (e) { 
-        console.log(e);
-    }
-    if (commitDate) {
-        commitCache.set(filePath, commitDate);
-    }
-    return commitDate;
-}
+    },
 
-export function clearCache() {
-    commitCache.clear();
+    clearCache: function () {
+        commitCache.clear();
+    }
 }
